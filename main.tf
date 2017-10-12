58caad74d57d94c597f5ad3337b36f4e915e9c71
Ashish K Thakur <ashishth@in.ibm.com>
2017-10-12 20:14:54 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "ibm_storage_file" "nfs_manager" {
+    capacity   = 20
+    datacenter = "${var.datacenter}"
+    iops       = 100
+    type       = "Performance"
+    hourly_billing = true
+    notes = "${var.name} swarm manager volume"
+}
+
+variable datacenter {
+    default = ""
+}
+
+variable name {
+    default = "d4ic"
+}
+
