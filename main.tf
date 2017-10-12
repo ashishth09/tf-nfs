d85ec353ebb6d777259ea328276a1e223fe84c76
Ashish Kumar Thakur <ashishth@in.ibm.com>
2017-10-12 20:36:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 resource "ibm_storage_file" "nfs_manager" {
     capacity   = 20
-    datacenter = "${var.datacenter}"
+    datacenter = "${module.nfs.datacenter}"
     iops       = 100
     type       = "Performance"
     hourly_billing = true
     notes = "${var.name} swarm manager volume"
 }
 
-variable datacenter {
-    default = ""
-}
 
 variable name {
     default = "d4ic"
